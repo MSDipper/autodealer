26a565984ef44665c711b849e6aba24498f5cd2f
MSDipper <102433692+MSDipper@users.noreply.github.com>
2023-04-16 00:48:12 +0300

    settings app

---
+++ movie/src/App.js
@@ -0,0 +1,14 @@
+import "./styles/css/bootstrap.css"
+import "./styles/css/popuo-box.css"
+import "./styles/css/style.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GGG</h1>
+    </div>
+  );
+}
+
+export default App;
